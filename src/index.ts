ab8eefc36795873c090a79405274fdd5103aa179
Rod Lewis <rod.lewis@watercorporation.com.au>
2022-07-24 09:57:24 +0800

    swap esbuild for vite

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export function pocketFn(jobProcessor: Function, ...props: any[]) {
-    let blob = new Blob([(() => {
+    let blob: Blob = new Blob([(() => {
         onmessage = ({ data: { jobProcessor, props } }) => {
             postMessage(new Function(`return ${decodeURI(jobProcessor)}`)()(props))
         };
     }).toString().replace(/^[^{]*{\s*/, '').replace(/\s*}[^}]*$/, '')], { type: 'text/javascript' });
-    let objUrl = (window.URL || window.webkitURL).createObjectURL(blob)
+    let objUrl = URL.createObjectURL(blob)
     return new Promise((resolve, reject) => {
         let worker = new Worker(objUrl);
         worker.onmessage = ({ data, isTrusted }) => {
             isTrusted ? resolve(data) : reject("not authorised")
             worker.terminate();
-            (window.URL || window.webkitURL).revokeObjectURL(objUrl)
+            URL.revokeObjectURL(objUrl)
         };
         worker.onerror = worker.onmessageerror = reject;
         worker.postMessage({ jobProcessor: encodeURI(jobProcessor.toString()), props });
